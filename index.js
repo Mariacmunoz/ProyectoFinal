ca67ec3afcc97ddd984a3e70a68b9d3ed24c91a1
Carlos Padilla <carlosfpm-@hotmail.com>
2023-10-31 18:44:01 -0500

    Se agrega la funcion de registrarse en la pagina

--- index.js
+++ index.js
@@ -6,7 +6,28 @@ app.get ('/', (req, res) => {
 });
 
 app.get ('/Registro', (req, res) => {
-    res.send ('Este es el modulo de registro'); 
+    res.send ('Este es el modulo de registro');
+
+    document.getElementById("registration-form").addEventListener("submit", function (e) {
+        e.preventDefault();
+      
+        const username = document.getElementById("username").value;
+        const email = document.getElementById("email").value;
+        const password = document.getElementById("password").value;
+      
+        // Verificar si el usuario ya existe en localStorage
+        if (localStorage.getItem(username)) {
+          alert("El nombre de usuario ya está en uso.");
+        } else {
+          // Almacenar el registro en localStorage
+          localStorage.setItem(username, JSON.stringify({ email, password }));
+          alert("Registro exitoso. Ahora puedes iniciar sesión.");
+        }
+      
+        // Limpia los campos del formulario
+        this.reset();
+      });
+      
 });
 
 app.get ('/About', (req, res) => {
